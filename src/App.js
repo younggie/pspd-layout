999de38f89b56f8c8d4ae9649e03fd8e2da1f6ba
Younggie Hong <younggie@gmail.com>
2020-02-25 21:49:32 +0900

    1st upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+//참고:  https://blog.logrocket.com/create-react-app-and-tailwindcss/
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className=" justify-start container flex flex-col mx-auto  bg-gray-100">
+        <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 sticky top-0">
+          <div className="container  text-right">메뉴아이콘들</div>
+          <div className="container  ">로고와 메뉴</div>
+        </div>
+        <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+          <div className="container h-auto">
+            <img src="http://www.peoplepower21.org/files/attach/images/1589375/a2c2367dad2e463ab6281e8930f21d1f.jpg?1582507697848"></img>
+          </div>
+          <div style={{ height: 700 }} className="bg-green-300">
+            본문
+          </div>
+        </div>
+        <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 sticky bottom-0">
+          푸터
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/tailwind.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
